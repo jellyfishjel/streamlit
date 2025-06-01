23e7674a980f2c09088e74469c411202df607384
jellyfishjel <meimeicute06@gmail.com>
2025-06-01 14:42:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ st.title("🌐 Multi-App Streamlit Dashboard")
 app_option = st.sidebar.selectbox("Chọn ứng dụng", ["App 1", "App 2", "App 3"])
 
 app_paths = {
-    "App 1": "app1/app.py",
-    "App 2": "app2/app.py",
-    "App 3": "app3/app.py"
+    "App 1": "pages/homepage.py",
+    "App 2": "pages/graphtab.py",
+    "App 3": "pages/overview.py"
 }
 
 def run_app(path):
