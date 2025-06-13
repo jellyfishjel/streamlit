504b923d975a1e4facf769095617624da606f7f3
jellyfishjel <meimeicute06@gmail.com>
2025-06-13 23:26:28 +0700

    Update homepage1.py

--- homepage1.py
+++ homepage1.py
@@ -94,4 +94,4 @@ cols_bot = st.columns([1, 3, 3, 3, 1])  # center 3 members
 for i, member in enumerate(bottom_row):
     with cols_bot[i + 1]:
         st.image(member["image"], width=250)
-        st.markdown(f"<div class='member-name'>{member['name']}</div>", unsafe_allow_html=True)
+        st.markdown(f"<div style='text-align:center; font-weight:bold; font-size:15px'>{member['name']}</div>", unsafe_allow_html=True)
